b0d418fa1208866958452ba09ae30e2bf422d1f3
Jeyakandhan <119617556+Jeyakandhan@users.noreply.github.com>
2022-12-02 13:39:10 +0530

    Create Assignment.py

---
+++ Assignment.py
@@ -0,0 +1,7 @@
+altitude=int(input("enter your current altitude :"))
+if altitude>= 10000:
+    print("keep going on")
+elif altitude >=5000 and altitude <10000:
+    print("by bringing it to 1000 ft")
+else:
+    print("landing your plane")
